c4d4bfb6e8a9433a80a881a0651ad37c6529a2de
AlexAndrei04 <bastidaflores@gmail.com>
2020-04-26 19:19:04 -0500

    Creacion de componentes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React from 'react';
 import './scss/main.scss'
 import Curso from './Curso'
 
+const cursos = [
+  {
+    'image': 'https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png',
+    'title': 'React desde Cero',
+    'price': '20'
+  },
+  {
+    'image': 'https://edteam-media.s3.amazonaws.com/courses/original/273c3c75-c615-44ee-bade-e41530524ac8.png',
+    'title': 'Que framework elegir',
+    'price': '0'
+  },
+  {
+    'image': 'https://edteam-media.s3.amazonaws.com/courses/original/8b7ef439-b9bc-485f-9608-d82d0d1a7a2b.png',
+    'title': 'Manejo del estado en React',
+    'price': '20'
+  },
+]
+
 const App = () => (
   <>
     <div className="main-banner img-container l-section" id="main-banner">
@@ -9,7 +27,7 @@ const App = () => (
         <div className="lg-cols-4 lg-x-2">
           <img alt='Banner' className="main-banner__img" src="https://images.pexels.com/photos/265614/pexels-photo-265614.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" />
           <div className="main-banner__data s-center">
-            <p className="t2 s-mb-0">Cursos de EDteam</p>
+            <h3 className="t2 s-mb-0">Cursos de EDteam</h3>
             <p>Tu futuro te esta esperando</p>
             <a href="https://ed.team/" className="button">Suscribirse</a>
           </div>
@@ -18,11 +36,9 @@ const App = () => (
     </div>
     
     <div className='ed-grid m-grid-3'>
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
+      {
+        cursos.map( curso => <Curso image={ curso.image } price={ curso.price } title={ curso.title } /> )
+      }
     </div>
   </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Curso = () => (
-  <article class="card">
-    <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2020-04/Untitled.png" alt="" />
+const Curso = ({title, image, price}) => (
+  <article className="card">
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+      <img alt={ title } src={ image } />
     </div>
-    <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 class="t5 s-mb-2 s-center">
-      Programación orientada a objetos con Go
-      </h3>
-      <div class="s-mb-2 s-main-center">
-      <div class="card__teacher s-cross-center">
-          <div class="card__avatar s-mr-1">
-          <div class="circle img-container">
-              <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/952327c3-2bd9-41d1-819e-9b5d7eb84c13.jpg" alt="" />
-          </div>
-          </div>
-          <span class="small">Alexys Lozada</span>
-      </div>
-      </div>
-      <div class="s-main-center">
-      <a class="button--ghost-alert button--tiny" href="https://ux.ed.team/cards.html">$ 20USD</a>
+    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+      <h3 className="t5 s-mb-2 s-center caca">{ title }</h3>
+      <div className="s-main-center">
+        <a className="button--ghost-alert button--tiny" href="https://ux.ed.team/cards.html">{ `$ ${price} USD` }</a>
       </div>
     </div>
   </article>
 )
+
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number
+}
+
+Curso.defaultProps = {
+  title: 'No sé encontró título',
+  image: 'https://doofindermedia.s3.amazonaws.com/blog/2019/08/29/110655-404-not-found-Doofinder.jpg',
+  price: '-'
+}
 
 export default Curso
